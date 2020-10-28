7df3b653f8b4110531ffe544dab10f8858318fa9
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2020-10-28 10:42:25 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.10

---
+++ src/KLDivergences.jl
@@ -0,0 +1,5 @@
+module KLDivergences
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using KLDivergences
+using Test
+
+@testset "KLDivergences.jl" begin
+    # Write your tests here.
+end
