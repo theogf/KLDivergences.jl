226539b8ad0843f32fdda998986c6d66341b7672
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2021-07-19 18:06:23 +0200

    Put more precise definition for MvNormal

--- src/multivariate.jl
+++ src/multivariate.jl
@@ -3,5 +3,12 @@ function KL(p::AbstractMvNormal, q::AbstractMvNormal)
         throw(DimensionMismatch("Distributions p and q have different dimensions $(length(p)) and $(length(q))"))
     Σp = cov(p)
     Σq = cov(q)
-    0.5 * (tr(Σq \ Σp) + invquad(Σq, mean(p) - mean(q)) - length(p) + logdet(Σq) - logdet(Σp))
+    Δμ = mean(p) - mean(q)
+    0.5 * (tr(Σq \ Σp) + dot(Δμ / Σq, Δμ) - length(p) + logdet(Σq) - logdet(Σp))
+end
+
+function KL(p::MvNormal, q::MvNormal)
+    length(p) == length(q) || 
+        throw(DimensionMismatch("Distributions p and q have different dimensions $(length(p)) and $(length(q))"))
+    0.5 * (tr(q.Σ \ p.Σ) + invquad(q.Σ, mean(p) - mean(q)) - length(p) + logdet(q.Σ) - logdet(p.Σ))
 end
