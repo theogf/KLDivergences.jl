5350da3463af18c786791cf486b7ee0f7480ba65
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2021-07-19 17:58:12 +0200

    Use AbstractMvNormal instead of MvNormal

--- src/multivariate.jl
+++ src/multivariate.jl
@@ -1,6 +1,7 @@
-function KL(p::MvNormal, q::MvNormal)
+function KL(p::AbstractMvNormal, q::AbstractMvNormal)
     length(p) == length(q) || 
         throw(DimensionMismatch("Distributions p and q have different dimensions $(length(p)) and $(length(q))"))
-    Σp = p.Σ; Σq = q.Σ
+    Σp = cov(p)
+    Σq = cov(q)
     0.5 * (tr(Σq \ Σp) + invquad(Σq, mean(p) - mean(q)) - length(p) + logdet(Σq) - logdet(Σp))
 end
